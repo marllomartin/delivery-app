9ecf8661eaa1c0e2a92f12b9bb08055442adb78a
MiguelNS <MiguelNS101@gmail.com>
2022-08-04 19:06:43 -0300

    feat(src/components): create Login Card

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
