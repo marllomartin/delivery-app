ae45c48b50601b8d6cd15061ac372731e43aabc0
marllomartin <marllon3636@gmail.com>
2022-08-04 18:42:49 -0300

    fix(src/database): adjust sequelize files

--- back-end/src/database/seeders/20220804194859-sales.js
+++ back-end/src/database/seeders/20220804194859-sales.js
@@ -1,11 +1,11 @@
 module.exports = {
   up: async (queryInterface, _Sequelize) => {
-    await queryInterface.bulkInsert('Sales',
+    await queryInterface.bulkInsert('sales',
       [{
         id: 1,
         user_id: 1,
         seller_id: 2,
-        total_price: 20,
+        total_price: 20.50,
         delivery_address: 'Rua Melancia',
         delivery_number: '20',
         sale_date: new Date('2022-08-01T19:58:00.000Z'),
@@ -15,7 +15,7 @@ module.exports = {
         id: 2,
         user_id: 2,
         seller_id: 1,
-        total_price: 35,
+        total_price: 35.20,
         delivery_address: 'Avenida Laranja',
         delivery_number: '30',
         sale_date: new Date('2022-08-01T19:58:00.000Z'),
@@ -25,6 +25,6 @@ module.exports = {
   },
 
   down: async (queryInterface, _Sequelize) => {
-    await queryInterface.bulkDelete('Sales', null, {});
+    await queryInterface.bulkDelete('sales', null, {});
   },
 };
