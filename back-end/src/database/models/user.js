6cd39cb1c84ea4a6ba76552ca941f296e86432b2
marllomartin <marllon3636@gmail.com>
2022-08-04 17:09:57 -0300

    feat(database): Sequelize setup

---
+++ back-end/src/database/models/user.js
@@ -0,0 +1,39 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    role: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    }
+  },
+    {
+      timestamps: false,
+    });
+
+  User.associate = (models) => {
+    User.hasMany(models.Sales, {
+      as: 'user',
+      foreignKey: 'userId',
+    });
+  };
+
+  return User;
+};
+
+module.exports = User;
---
+++ back-end/src/database/seeders/20220804194859-sales.js
@@ -0,0 +1,30 @@
+module.exports = {
+  up: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkInsert('Sales',
+      [{
+        id: 1,
+        user_id: 1,
+        seller_id: 2,
+        total_price: 20,
+        delivery_address: 'Rua Melancia',
+        delivery_number: '20',
+        sale_date: new Date('2022-08-01T19:58:00.000Z'),
+        status: 'em andamento',
+      },
+      {
+        id: 2,
+        user_id: 2,
+        seller_id: 1,
+        total_price: 35,
+        delivery_address: 'Avenida Laranja',
+        delivery_number: '30',
+        sale_date: new Date('2022-08-01T19:58:00.000Z'),
+        status: 'finalizada',
+      },
+      ], { timestamps: false });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkDelete('Sales', null, {});
+  },
+};
