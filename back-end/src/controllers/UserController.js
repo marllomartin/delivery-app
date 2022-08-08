e00a5af23a80e9324c579eeef96147c9c721032b
marllomartin <marllon3636@gmail.com>
2022-08-08 16:56:47 -0300

    feat(controllers/UserController): create register user controller

--- back-end/src/controllers/UserController.js
+++ back-end/src/controllers/UserController.js
@@ -3,12 +3,20 @@ const UserService = require('../services/UserService');
 
 const login = async (req, res) => {
   try {
-    const { email, password } = req.body;
-    const result = await UserService.login(email, password);
+    const result = await UserService.login(req.body);
     return res.status(StatusCodes.OK).json(result);
   } catch (Error) {
     return res.status(StatusCodes.NOT_FOUND).send({ message: Error.message });
   }
 };
 
-module.exports = { login };
+const register = async (req, res) => {
+  try {
+    const result = await UserService.register(req.body);
+    return res.status(StatusCodes.CREATED).json(result);
+  } catch (Error) {
+    return res.status(StatusCodes.NOT_FOUND).send({ message: Error.message });
+  }
+}
+
+module.exports = { login, register };
