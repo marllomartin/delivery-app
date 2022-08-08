15192396bf7228bfa3426f6461e4ba68323e838f
yang vieira <yangflix3@gmail.com>
2022-08-08 18:03:54 -0400

    :sparkles: req 11

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
+import Product from './pages/Product';
 import Register from './pages/Register';
 // import './App.css';
 
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={ <Navigate replace to="/login" /> } />
         <Route path="/login" element={ <Login /> } />
         <Route path="/register" element={ <Register /> } />
+        <Route path="/customer/products" element={ <Product /> } />
       </Routes>
     </div>
   );
