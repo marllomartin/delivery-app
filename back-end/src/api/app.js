ec0fc4f10941593f49357dc65f10ee8dee05d94f
marllomartin <marllon3636@gmail.com>
2022-08-08 16:57:15 -0300

    feat(src/api): add register route to app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const UserController = require('../controllers/UserController');
+const ProductController = require('../controllers/ProductController');
 
 const app = express();
 
 app.use(express.json());
 
 app.post('/login', UserController.login);
+app.post('/register', UserController.register);
 
 module.exports = app;
