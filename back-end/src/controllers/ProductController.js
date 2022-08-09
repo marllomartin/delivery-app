3cd488849b2199255f4b29d22423b1afa5e69d5b
Giovanna <giovannaelizz@hotmail.com>
2022-08-09 14:58:57 -0300

    fix bugs

--- back-end/src/controllers/ProductController.js
+++ back-end/src/controllers/ProductController.js
@@ -11,12 +11,12 @@ const findAll = async (req, res) => {
 };
 const findById = async (req, res) => {
   try {
-    const  { id } = req.params;
-    const result = await ProductService.findById(id)
+    const { id } = req.params;
+    const result = await ProductService.findById(id);
     return res.status(StatusCodes.OK).send(result);
   } catch (Error) {
     return res.status(StatusCodes.NOT_FOUND).send({ message: Error.message });
   }
-}
+};
 
 module.exports = { findAll, findById };
