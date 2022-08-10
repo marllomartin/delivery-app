ad7bdd84d0ce2b724636be3008e9379ca73d2592
MiguelNS <MiguelNS101@gmail.com>
2022-08-10 15:32:16 -0300

    feat(src/components): create components, fix

--- front-end/src/pages/Product.jsx
+++ front-end/src/pages/Product.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NavHeader from '../components/NavHeader';
 import ProductCard from '../components/ProductCard';
 
 function ProductsPage() {
+  const [productsList, setProductsList] = useState([]);
+  const login = async () => fetch('http://localhost:3001/products', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((res) => {
+      setProductsList(res);
+    });
+
+  useEffect(() => {
+    login();
+  }, []);
+
   return (
     <>
       <div>
         <NavHeader />
       </div>
       <main>
-        <ProductCard />
+        {productsList.map((prod) => (
+          <ProductCard key={ `prodCard${prod.id}` } data={ prod } />
+        ))}
       </main>
     </>
   );
