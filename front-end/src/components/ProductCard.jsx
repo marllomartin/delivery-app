781280aae93e35df7f90c46cbe5f247fcfdee666
MiguelNS <MiguelNS101@gmail.com>
2022-08-11 15:12:03 -0300

    Fix

--- front-end/src/components/ProductCard.jsx
+++ front-end/src/components/ProductCard.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useCart } from 'react-use-cart';
 import PropTypes from 'prop-types';
 import AddProductButton from './AddProductButton';
 import RemoveProductButton from './RemoveProductButton';
 import ProductQuantity from './ProductQuantity';
 
 function ProductCard({ data }) {
+  const { getItem } = useCart();
+  function getQt() {
+    const { id } = data;
+
+    const qt = getItem(id);
+    if (qt === undefined) {
+      return 0;
+    }
+    return qt.quantity;
+  }
+
+  const [prodQt, setProdQt] = useState(getQt());
+
   const { urlImage, name, id, price } = data;
   return (
     <div>
@@ -13,15 +27,23 @@ function ProductCard({ data }) {
         alt={ name }
         data-testid={ `customer_products__img-card-bg-image-${id}` }
       />
-      <p data-testid={ `customer_products__element-card-title-${id}` }>
-        {name}
-      </p>
+      <p data-testid={ `customer_products__element-card-title-${id}` }>{name}</p>
       <p data-testid={ `customer_products__element-card-price-${id}` }>
         {price.replace(/\./, ',')}
       </p>
-      <AddProductButton key={ `add${id}` } data={ data } />
-      <ProductQuantity key={ `pq${id}` } data={ data } />
-      <RemoveProductButton key={ `rm${id}` } data={ data } />
+      <AddProductButton
+        key={ `add${id}` }
+        data={ data }
+        quant={ prodQt }
+        setQuant={ setProdQt }
+      />
+      <ProductQuantity key={ `pq${id}` } data={ data } quant={ prodQt } />
+      <RemoveProductButton
+        key={ `rm${id}` }
+        data={ data }
+        quant={ prodQt }
+        setQuant={ setProdQt }
+      />
     </div>
   );
 }
--- front-end/src/components/ProductQuantity.jsx
+++ front-end/src/components/ProductQuantity.jsx
@@ -1,17 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useCart } from 'react-use-cart';
+import { useForm } from 'react-hook-form';
 
 function ProductQuantity({ data }) {
-  const { urlImage, name, id, price } = data;
+  const {
+    register,
+    handleSubmit,
+    getValues,
+  } = useForm({ mode: 'onChange' });
+
+  const { updateItemQuantity, getItem, addItem } = useCart();
+  const { id } = data;
+
+  function getQt() {
+    const qt = getItem(id);
+    if (qt === undefined) {
+      return 0;
+    }
+    return qt.quantity;
+  }
+
+  function onChange() {
+    const value = getValues('quantity');
+    const item = getItem(id);
+    if (item === undefined) {
+      addItem(data, parseInt(value, 10));
+    } else {
+      updateItemQuantity(id, parseInt(value, 10));
+    }
+  }
+
   return (
-    <label htmlFor="quantity">
-      Qt:
+    <form onSubmit={ handleSubmit() }>
       <input
-        value={ 0 }
         type="number"
         data-testid={ `customer_products__input-card-quantity-${id}` }
+        { ...register('quantity', {
+          min: 0,
+          value: getQt(),
+          onChange: () => onChange(),
+        }) }
       />
-    </label>
+    </form>
   );
 }
 
