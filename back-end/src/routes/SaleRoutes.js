bc1723a9e5f4e96ea68ff54ed51eaaa1223761a6
RonnyMv <ronnyjmvls>
2022-08-15 13:07:50 -0300

    feat(routes): implement PATCH /id update

--- back-end/src/routes/SaleRoutes.js
+++ back-end/src/routes/SaleRoutes.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
 
-const { findAll, findById, create } = require('../controllers/SaleController');
+const { findAll, findById, create, update } = require('../controllers/SaleController');
 const authToken = require('../middlewares/authToken');
+const validateUpdateSale = require('../middlewares/validateUpdate');
 
 router.get('/orders', findAll);
 router.get('/orders/:id', findById);
 router.post('/orders', authToken, create);
+
+router.patch('/:id', validateUpdateSale, update);
 
 module.exports = router;
