c076794ca4db84426d5a55b85819c8a3dd1b3c14
marllomartin <marllon3636@gmail.com>
2022-08-15 16:18:30 -0300

    refactor(services): adjust update service

--- back-end/src/services/SaleService.js
+++ back-end/src/services/SaleService.js
@@ -42,9 +42,9 @@ const create = async (userId, data) => {
   return newSale;
 };
 
-const update = async (data, id) => {
-  await sale.update({ status: data.status }, { where: { id } });
-  return { message: 'update successfully' };
+const update = async (status, id) => {
+  await sale.update({ status }, { where: { id } });
+  return { message: `Sale ${id} status was updated to '${status}' `};
 };
 
 module.exports = { findAll, getById, create, update };
