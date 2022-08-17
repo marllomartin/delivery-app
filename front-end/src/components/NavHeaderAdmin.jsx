698ab6eb3de1eb02a7071a3db31649f3a4b7a971
MiguelNS <MiguelNS101@gmail.com>
2022-08-17 15:29:21 -0300

    feat(src/pages): create adm page

--- front-end/src/components/NavHeaderAdmin.jsx
+++ front-end/src/components/NavHeaderAdmin.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-function NavHeaderSeller() {
+function NavHeaderAdmin() {
   const history = useNavigate();
   const [userData] = useState(JSON.parse(localStorage.getItem('user')));
 
@@ -22,9 +22,9 @@ function NavHeaderSeller() {
       <nav>
         <a
           data-testid="customer_products__element-navbar-link-orders"
-          href="/seller/orders/"
+          href="/admin/manage/"
         >
-          MEUS PEDIDOS
+          Gerenciar Usuarios
         </a>
         <a
           data-testid="customer_products__element-navbar-user-full-name"
@@ -44,4 +44,4 @@ function NavHeaderSeller() {
   );
 }
 
-export default NavHeaderSeller;
+export default NavHeaderAdmin;
